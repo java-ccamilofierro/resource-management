ff9d1d71a20a4d0f6b63aeb0781608c45973a733
Cristian Fierro <ccamilofierro@gmail.com>
2024-09-10 13:53:49 -0500

    creation of the model, repository and service

---
+++ src/main/java/com/example/resourcemanagement/model/User.java
@@ -0,0 +1,30 @@
+package com.example.resourcemanagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
+    private String username;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    private String password;
+
+    @Email(message = "Debe proporcionar un correo válido")
+    @NotBlank(message = "El correo no puede estar vacío")
+    private String email;
+
+    private String role;
+}
