d830a90c3239261b418d606f0d8fd11ad8578d15
Cristian Fierro <ccamilofierro@gmail.com>
2024-09-10 21:50:10 -0500

    creating global exceptions

--- src/main/java/com/example/resourcemanagement/controller/UserController.java
+++ src/main/java/com/example/resourcemanagement/controller/UserController.java
@@ -4,12 +4,14 @@ import com.example.resourcemanagement.model.User;
 import com.example.resourcemanagement.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Validated
 public class UserController {
 
     @Autowired
---
+++ src/main/java/com/example/resourcemanagement/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.resourcemanagement.exceptions;
+
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
