871eed36d95b41f2e33b83bf1e9f9af174f2fde3
Cristian Fierro <ccamilofierro@gmail.com>
2024-09-10 21:57:12 -0500

    updating business logic in the service

--- src/main/java/com/example/resourcemanagement/service/UserService.java
+++ src/main/java/com/example/resourcemanagement/service/UserService.java
@@ -2,7 +2,9 @@ package com.example.resourcemanagement.service;
 
 import com.example.resourcemanagement.model.User;
 import com.example.resourcemanagement.repository.UserRepository;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,16 +15,23 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
 
     public User getUserById(Long id){
         return userRepository.findById(id).
-                orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     public User createUser(User user){
+        if(userRepository.findByUserName(user.getUsername()).isPresent()){
+            throw new ValidationException("Username alredy exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -30,11 +39,11 @@ public class UserService {
         User existingUser = getUserById(id);
         existingUser.setUsername(user.getUsername());
         existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(existingUser);
     }
 
     public void deleteUser(Long id){
-        User user = getUserById(id);
-        userRepository.delete(user);
+        userRepository.deleteById(id);
     }
 }
